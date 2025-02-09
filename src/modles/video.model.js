06e40b515d84153773c1284aac77ccf06af15cbb
bilalarshad-1 <bilalarshad03054@gmail.com>
2025-02-10 03:23:30 +0500

    jwt and bcrypt

---
+++ src/modles/video.model.js
@@ -0,0 +1,55 @@
+import mongoose, {Schema}  from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+
+    {
+        
+        videoFile: {
+            type: String, //Cloudinaru URL
+            require: true,
+        },
+        thumbnail: {
+            type: String, //Cloudinaru URL
+            require: true,
+        },
+        title: {
+            type: String, 
+            require: true,
+        },
+        description: {
+            type: String, 
+            require: true,
+        },
+        duration: {
+            type: Number, //Cloudinaru URL
+            require: true,
+
+        },
+        view: {
+            type: Number, //Cloudinaru URL
+            default: 0
+
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+            
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+
+        
+
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", videoSchema)
