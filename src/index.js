e6b935b4a131b162e4fe410930063aa0d571dcc1
bilalarshad-1 <bilalarshad03054@gmail.com>
2025-02-12 15:52:29 +0500

    changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
-dotenv.config();  // Load env variables before anything else
-
+dotenv.config(); 
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
